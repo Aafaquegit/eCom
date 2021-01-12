ea6e41e3bdff302f7af18e91c1a1bca3457f4f8a
Aafaque123 <aafaque354@gmail.com>
2021-01-12 18:14:02 +0530

    Made the Settings Activity page(Settings Activity) which lets us to update a users profile.

--- app/src/main/java/com/example/ecom/HomeActivity.java
+++ app/src/main/java/com/example/ecom/HomeActivity.java
@@ -79,6 +79,7 @@ public class HomeActivity extends AppCompatActivity
         CircleImageView profileImageView = headerView.findViewById(R.id.user_profile_image);
 
         userNameTextView.setText(Prevalent.currentOnlineUser.getName());
+        Picasso.get().load(Prevalent.currentOnlineUser.getImage()).placeholder(R.drawable.profile).into(profileImageView);
 
         recyclerView = findViewById(R.id.recycler_menu);
         recyclerView.setHasFixedSize(true);
@@ -167,7 +168,8 @@ public class HomeActivity extends AppCompatActivity
         }
         else if (id == R.id.nav_settings)
         {
-
+            Intent intent = new Intent(HomeActivity.this, SettingsActivity.class);
+            startActivity(intent);
         }
         else if (id == R.id.nav_logout)
         {
